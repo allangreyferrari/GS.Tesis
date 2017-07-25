6282f3c6dec86f5edce51a6262d51721122c967c
Irvin <nvr.amb@gmail.com>
2017-07-24 23:19:38 -0500

    Listado y Asignacion de paquete

--- GS.module.resources/scripts/ssii/ejecucion/controller-inspeccionesfisicas.js
+++ GS.module.resources/scripts/ssii/ejecucion/controller-inspeccionesfisicas.js
@@ -34,27 +34,57 @@ function SetToday(inputDATE) {
 }
 
 
-function Relacionar() {    
-    $("#modalRelacionarHeader").html("<i class=\"fa fa-handshake-o\">&nbsp;</i> Relacionar");
-    $("#modalRelacionar").modal("show");
+function Relacionar() {
 
-    $("#modalRelacionarFecha").mask("99/99/9999", { placeholder: 'DD/MM/YYYY' });
-    $('#modalRelacionarFecha').datepicker({
-        format: "dd/mm/yyyy",
-        language: "es",
-        calendarWeeks: true,
-        multidateSeparator: "/",
-        beforeShowYear: function (date) {
-            if (date.getFullYear() == 2007) {
-                return false;
-            }
-        }
+    var params = JSON.stringify({
+        "key": "bFHXyZa3Tmk=",
+        "parametros":
+            [
+                localStorage.getItem('session')
+            ],
+        "cryp": []
     });
 
-    SetToday("modalRelacionarFecha");
+    $.ajax({
+        type: "POST",
+        url: wsnode + "wsCommon.svc/ListarBasicTen",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: params,
+        async: true,
+        processData: true,
+        cache: false,
+        success: function (response) {
+            if (response.Transaction.Type == 1)
+                showError(response.Transaction.Message);
+            if (response.NroRows > 0)
+                showError("Ud. ya tiene asignado un paquete para el día");
+            else {
+                $("#modalRelacionarHeader").html("<i class=\"fa fa-handshake-o\">&nbsp;</i> Relacionar");
+                $("#modalRelacionar").modal("show");
 
-    $("#gridrelacionar").empty();
-    $("#modalRelacionarPaquete").val("");
+                $("#modalRelacionarFecha").mask("99/99/9999", { placeholder: 'DD/MM/YYYY' });
+                $('#modalRelacionarFecha').datepicker({
+                    format: "dd/mm/yyyy",
+                    language: "es",
+                    calendarWeeks: true,
+                    multidateSeparator: "/",
+                    beforeShowYear: function (date) {
+                        if (date.getFullYear() == 2007) {
+                            return false;
+                        }
+                    }
+                });
+
+                SetToday("modalRelacionarFecha");
+
+                $("#gridrelacionar").empty();
+                $("#modalRelacionarPaquete").val("");
+            }
+        },
+        error: function (response) {
+        }
+    });
 }
 
 function keypress(e) {
@@ -67,6 +97,7 @@ function keypress(e) {
             "key": "BQoZCyuSGbA=",
             "parametros":
                 [
+                    $('#modalRelacionarFecha').val(),
                     $('#modalRelacionarPaquete').val(),
                     localStorage.getItem('session')
                 ],
@@ -104,7 +135,7 @@ function keypress(e) {
 
         $("#gridrelacionar").kendoGrid({
             dataSource: dsgrid,
-            height: 200,
+            height: 300,
 
             rowTemplate:
                         "<tr>" +
@@ -160,17 +191,52 @@ function keypress(e) {
     }
 }
 
+function GuardarRelacion() {
+
+    var params = JSON.stringify({
+        "key": "iUeaL4yLC3g=",
+        "parametros":
+            [
+                $('#modalRelacionarFecha').val(),
+                $('#modalRelacionarPaquete').val(),
+                localStorage.getItem('session')
+            ],
+        "cryp": []
+    });
+    $.ajax({
+        type: "POST",
+        url: wsnode + "wsCommon.svc/EjecutarTransaction",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: params,
+        async: true,
+        processData: false,
+        cache: false,
+        success: function (transaction) {
+            if (transaction.Type == 1)
+                showSuccess(transaction.Message);
+            else
+                showError(transaction.Message);
+            $("#modalRelacionar").modal("hide");
+            ListarInspeccionesFiltro();
+        },
+        error: function (transaction) {
+            showError(JSON.stringify(transaction.Message));
+        }
+    });
+    $("#modalRelacionar").modal("hide");
+}
+
 function ListarInspeccionesFiltro() {
     $("#gridpadre").empty();
     var params = JSON.stringify({
-        "key": "VTO/98UvYYI=",
+        "key": "U2uOI7WhOsw=",
         "parametros":
             [
                 $('#filtroFecha').val(),
-                $('#filtroFecha').val(),
-                $('#filtroVolante').val(),
-                $('#filtroDUA').val(),
                 $('#filtroPaquete').val(),
+                $('#filtroDua').val(),
+                $('#filtroVolante').val(),
                 localStorage.getItem('session')
             ],
         "cryp": []
@@ -226,6 +292,7 @@ function ListarInspeccionesFiltro() {
                     "<td><center>#: v06 #</center></td>" +
                     "<td><center>#: v07 #</center></td>" +
                     "<td><center>#: v08 #</center></td>" +
+                    "<td><center>#: v09 #</center></td>" +
                     "</tr>",
         altRowTemplate:
                     "<tr>" +
@@ -242,6 +309,7 @@ function ListarInspeccionesFiltro() {
                     "<td><center>#: v06 #</center></td>" +
                     "<td><center>#: v07 #</center></td>" +
                     "<td><center>#: v08 #</center></td>" +
+                    "<td><center>#: v09 #</center></td>" +
                     "</tr>",
         group: {
             field: "saludo"
@@ -312,7 +380,7 @@ function ListarInspeccionesFiltro() {
 
 function VerDetalle(id) {
     var params = JSON.stringify({
-        "key": "OEsAllGD/0A=",
+        "key": "uLxG8DprxXg=",
         "parametros":
             [
                 parseInt(id),
@@ -339,20 +407,20 @@ function VerDetalle(id) {
             $("#modalDUAHeader").html("<i class=\"fa fa-plus\">&nbsp;</i> Modificar DUA");
             $("#modalDetalleId").val(response.Rows[0].v01);
 
-            $("#modalDetalleGuia").val(response.Rows[0].v04)
-            $("#modalDetalleVolante").val(response.Rows[0].v05);
-            $("#modalDetalleBultos").val(response.Rows[0].v07);
-            $("#modalDetalleKilos").val(response.Rows[0].v01);
-            $("#modalDetalleAgente").val(response.Rows[0].v01);
-            $("#modalDetalleLineaAerea").val(response.Rows[0].v01);
-            $("#modalDetalleCliente").val(response.Rows[0].v01);
-            $("#modalDetalleDescripcion").val(response.Rows[0].v01);
-            $("#modalDetalleUbicacion").val(response.Rows[0].v01);
+            $("#modalDetalleGuia").val(response.Rows[0].v02)
+            $("#modalDetalleVolante").val(response.Rows[0].v03);
+            $("#modalDetalleBultos").val(response.Rows[0].v04);
+            $("#modalDetalleKilos").val(response.Rows[0].v05);
+            $("#modalDetalleAgente").val(response.Rows[0].v06);
+            $("#modalDetalleLineaAerea").val(response.Rows[0].v07);
+            $("#modalDetalleCliente").val(response.Rows[0].v08);
+            $("#modalDetalleDescripcion").val(response.Rows[0].v09);
+            $("#modalDetalleUbicacion").val(response.Rows[0].v10);
 
-            $("#modalDetalleDua").val(response.Rows[0].v03);
-            $("#modalDetalleTurno").val(response.Rows[0].v01);
-            $("#modalDetalleFechaAtencion").val(response.Rows[0].v09);
-            $("#modalDetalleHorario").val(response.Rows[0].v01);
+            $("#modalDetalleDua").val(response.Rows[0].v11);
+            $("#modalDetalleTurno").val(response.Rows[0].v12);
+            $("#modalDetalleFechaAtencion").val(response.Rows[0].v13);
+            $("#modalDetalleHorario").val(response.Rows[0].v14);
 
             $("#modalDetalleHeader").html("<i class=\"fa fa-eye\">&nbsp;</i> Detalle");
             $("#modalDetalle").modal("show");
@@ -363,116 +431,162 @@ function VerDetalle(id) {
     });
 }
 
-function RevisionDocumentaria() {
-    $("#modalRevisionHeader").html("<i class=\"fa fa-folder-open-o\">&nbsp;</i> Revisión documentaria");
-    $("#modalRevision").modal("show");
+function RevisionDocumentaria(id) {
+    if (InspeccionAsignadaAUsuario(id)) {
 
-    $("#gridRevisionDocumentaria").kendoGrid({
-        dataSource: [
-            { saludo: "Jane Doe", age: 30 },
-            { saludo: "John Doe", age: 33 }
-        ],
-        height: 200,
+        $("#modalRevisionHeader").html("<i class=\"fa fa-folder-open-o\">&nbsp;</i> Revisión documentaria");
+        $("#modalRevision").modal("show");
 
-        rowTemplate:
-                    "<tr>" +
-                    "<td><center>#: saludo #</center></td>" +
-                    //"<td><center>#: v02 #</center></td>" +
-                    //"<td><center>#: v03 #</center></td>" +
-                    //"<td><center>#: v04 #</center></td>" +
-                    "</tr>",
-        altRowTemplate:
-                    "<tr>" +
-                    "<td><center>#: saludo #</center></td>" +
-                    //"<td><center>#: v02 #</center></td>" +
-                    //"<td><center>#: v03 #</center></td>" +
-                    //"<td><center>#: v04 #</center></td>" +                  
-                    "</tr>",
-        group: {
-            field: "saludo"
-        },
-        groupable: false,
-        sortable: true,
-        selectable: true,
-        resizable: true,
-        columns: [
-        {
-            field: "v02",
-            title: "<center>Nombre</center>",
-            width: 100
-        },
-        {
-            field: "v03",
-            title: "<center>Fecha de carga</center>",
-            width: 70
-        },
-        {
-            field: "v04",
-            title: "<center>Estado</center>",
-            width: 70
-        },
-        {
-            field: "v05",
-            title: "<center>Ver</center>",
-            width: 70
-        }
-        ]
-    });
+        $("#gridRevisionDocumentaria").kendoGrid({
+            dataSource: [
+                { saludo: "Jane Doe", age: 30 },
+                { saludo: "John Doe", age: 33 }
+            ],
+            height: 200,
+
+            rowTemplate:
+                        "<tr>" +
+                        "<td><center>#: saludo #</center></td>" +
+                        //"<td><center>#: v02 #</center></td>" +
+                        //"<td><center>#: v03 #</center></td>" +
+                        //"<td><center>#: v04 #</center></td>" +
+                        "</tr>",
+            altRowTemplate:
+                        "<tr>" +
+                        "<td><center>#: saludo #</center></td>" +
+                        //"<td><center>#: v02 #</center></td>" +
+                        //"<td><center>#: v03 #</center></td>" +
+                        //"<td><center>#: v04 #</center></td>" +                  
+                        "</tr>",
+            group: {
+                field: "saludo"
+            },
+            groupable: false,
+            sortable: true,
+            selectable: true,
+            resizable: true,
+            columns: [
+            {
+                field: "v02",
+                title: "<center>Nombre</center>",
+                width: 100
+            },
+            {
+                field: "v03",
+                title: "<center>Fecha de carga</center>",
+                width: 70
+            },
+            {
+                field: "v04",
+                title: "<center>Estado</center>",
+                width: 70
+            },
+            {
+                field: "v05",
+                title: "<center>Ver</center>",
+                width: 70
+            }
+            ]
+        });
+    } else {
+        showError("Esta inspección ha sido asignada a otro usuario");
+    }
 }
 
 function RevisionFisica() {
-    $("#modalRevisionHeader").html("<i class=\"fa fa-cube\">&nbsp;</i> Revisión física");
-    $("#modalRevision").modal("show");
+    if (InspeccionAsignadaAUsuario(id)) {
+        $("#modalRevisionHeader").html("<i class=\"fa fa-cube\">&nbsp;</i> Revisión física");
+        $("#modalRevision").modal("show");
 
-    $("#gridRevisionDocumentaria").kendoGrid({
-        dataSource: [
-            { saludo: "Jane Doe", age: 30 },
-            { saludo: "John Doe", age: 33 }
-        ],
-        height: 200,
+        $("#gridRevisionDocumentaria").kendoGrid({
+            dataSource: [
+                { saludo: "Jane Doe", age: 30 },
+                { saludo: "John Doe", age: 33 }
+            ],
+            height: 200,
 
-        rowTemplate:
-                    "<tr>" +
-                    "<td><center>#: saludo #</center></td>" +
-                    //"<td><center>#: v02 #</center></td>" +
-                    //"<td><center>#: v03 #</center></td>" +
-                    //"<td><center>#: v04 #</center></td>" +
-                    "</tr>",
-        altRowTemplate:
-                    "<tr>" +
-                    "<td><center>#: saludo #</center></td>" +
-                    //"<td><center>#: v02 #</center></td>" +
-                    //"<td><center>#: v03 #</center></td>" +
-                    //"<td><center>#: v04 #</center></td>" +                  
-                    "</tr>",
-        group: {
-            field: "saludo"
-        },
-        groupable: false,
-        sortable: true,
-        selectable: true,
-        resizable: true,
-        columns: [
-        {
-            field: "v02",
-            title: "<center>Nombre</center>",
-            width: 100
-        },
-        {
-            field: "v03",
-            title: "<center>Fecha de carga</center>",
-            width: 70
-        },
-        {
-            field: "v04",
-            title: "<center>Estado</center>",
-            width: 70
-        },
-        {
-            field: "v05",
-            title: "<center>Ver</center>",
-            width: 70
-        }
-        ]
+            rowTemplate:
+                        "<tr>" +
+                        "<td><center>#: saludo #</center></td>" +
+                        //"<td><center>#: v02 #</center></td>" +
+                        //"<td><center>#: v03 #</center></td>" +
+                        //"<td><center>#: v04 #</center></td>" +
+                        "</tr>",
+            altRowTemplate:
+                        "<tr>" +
+                        "<td><center>#: saludo #</center></td>" +
+                        //"<td><center>#: v02 #</center></td>" +
+                        //"<td><center>#: v03 #</center></td>" +
+                        //"<td><center>#: v04 #</center></td>" +                  
+                        "</tr>",
+            group: {
+                field: "saludo"
+            },
+            groupable: false,
+            sortable: true,
+            selectable: true,
+            resizable: true,
+            columns: [
+            {
+                field: "v02",
+                title: "<center>Nombre</center>",
+                width: 100
+            },
+            {
+                field: "v03",
+                title: "<center>Fecha de carga</center>",
+                width: 70
+            },
+            {
+                field: "v04",
+                title: "<center>Estado</center>",
+                width: 70
+            },
+            {
+                field: "v05",
+                title: "<center>Ver</center>",
+                width: 70
+            }
+            ]
+        });
+    } else {
+        showError("Esta inspección ha sido asignada a otro usuario");
+    }
+}
+
+function InspeccionAsignadaAUsuario(id) {
+
+    var asignado = false;
+
+    var params = JSON.stringify({
+        "key": "apqv+NAK2Qo=",
+        "parametros":
+            [
+                parseInt(id),
+                localStorage.getItem('session')
+            ],
+        "cryp": []
     });
+
+    $.ajax({
+        type: "POST",
+        url: wsnode + "wsCommon.svc/ListarBasicFive",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: params,
+        async: true,
+        processData: true,
+        cache: false,
+        success: function (response) {
+            showError(response.NroRows);
+            if (response.Transaction.Type == 1)
+                showError(response.Transaction.Message);
+            if (response.NroRows > 0)
+                asignado = true;
+        },
+        error: function (response) {            
+        }
+    });
+
+    return asignado;
 }
